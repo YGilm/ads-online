f0dd05395b9f65334195f943a3812bb09ae7e987
yury gilm <ygilm@bk.ru>
2024-04-20 22:02:46 +0300

    Added new views for Ad and Review

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Ad, Review
+from .serializers import AdSerializer, ReviewSerializer
+from .filters import AdFilter
+from .paginators import AdPagination
 
-# Create your views here.
+
+class AdViewSet(viewsets.ModelViewSet):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdFilter
+    pagination_class = AdPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
