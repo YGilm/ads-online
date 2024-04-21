f21281947845c815766254a759c5cd5917260d9c
yury gilm <ygilm@bk.ru>
2024-04-21 22:30:09 +0300

    Admin panel configured

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'first_name', 'phone', 'role', 'is_active', 'is_staff']
+    list_filter = ('is_staff', 'is_active', 'role')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'phone', 'image')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'role', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'first_name', 'phone', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+    search_fields = ('email', 'first_name', 'phone')
+    ordering = ('email',)
