6ae3f3d6da17f339fb63c4bb05076e3ac7aff4f9
yury gilm <ygilm@bk.ru>
2024-04-21 17:17:33 +0300

    Changed views

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,25 +1,50 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from .models import Ad, Review
+from .permissions import IsOwnerOrAdmin
 from .serializers import AdSerializer, ReviewSerializer
 from .filters import AdFilter
 from .paginators import AdPagination
 
 
-class AdViewSet(viewsets.ModelViewSet):
+class AdList(ListCreateAPIView):
+    """
+    Представление для списка объявлений и создания нового объявления.
+    Поддерживает фильтрацию, пагинацию и создание объявления с автоматическим назначением автора.
+    """
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = AdFilter
     pagination_class = AdPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
+class AdDetail(RetrieveUpdateDestroyAPIView):
+    """
+    Представление для получения, обновления и удаления конкретного объявления.
+    Обеспечивает детальный просмотр, редактирование и удаление объявления.
+    """
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrAdmin]
+
+
 class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Представление для работы с отзывами на объявления.
+    Поддерживает создание, просмотр, редактирование и удаление отзывов.
+    Автоматически назначает автора отзыва при создании.
+    """
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
